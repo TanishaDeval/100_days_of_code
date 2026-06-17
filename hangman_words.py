f46182b340459aa99cffb502d2d018b97cea01ce
Tanisha Deval <tanishadeval@gmail.com>
2026-06-17 19:54:08 +0530

    Add files via upload

---
+++ hangman_words.py
@@ -0,0 +1,16 @@
+hangman_words_list = [
+    "bamboozle", "banjo", "blizzard", "bubblegum", "chuckle",
+    "confetti", "cupcake", "doodle", "firecracker", "flamingo",
+    "giggle", "gossip", "jellyfish", "jigsaw", "jodhpurs",
+    "jogging", "kazoo", "luck", "marbles", "mischief",
+    "monkey", "mudslide", "mustache", "noodles", "pancake",
+    "pirate", "popcorn", "pumpkin", "puppy", "sandbox",
+    "scuba", "shuffle", "skydive", "spaghetti", "waffle",
+    "whiskey", "wombat", "zombie","invisible","cuisine",
+    "awkward", "buzzard", "crypt", "fizzled", "fuzzy",
+    "galaxy", "haphazard", "jinx", "kickbox", "luxury",
+    "mystify", "oxygen", "puzzling", "queue", "quizzical",
+    "rhythm", "scratch", "sphinx", "subway", "unzip",
+    "vaporize", "voodoo", "wave", "yacht", "zephyr"
+]
+
